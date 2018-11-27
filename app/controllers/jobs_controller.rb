cee50a4cdfbc28dcd510b9ded2d8bd6bce18c58d
Josh Conduah <jconduah729@yahoo.com>
2018-11-27 15:28:21 +0900

    created show page

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,15 +1,20 @@
 class JobsController < ApplicationController
   def index
+
     @jobs = Job.all
   end
 
   def show
+    # @job.rating = Booking.find(params)
+    @job = Job.find(params[:id])
   end
 
   def new
+    @job = Job.new
   end
 
   def create
+    @job = Job.new(params[:id])
   end
 
   def edit
